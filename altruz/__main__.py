230746978126fa4697fdc63357cc37d8458ac065
XkshaT7678 <73880884+XkshaT7678@users.noreply.github.com>
2021-12-29 08:09:51 +0530

    Update __main__.py

--- altruz/__main__.py
+++ altruz/__main__.py
@@ -1,24 +1,24 @@
 import asyncio
 
 from pyrogram import idle
-from alturz import ALTURZ
-from alturz.modules import *
-from alturz.core.startup_checks import check_or_set_log_channel, check_arq_api
-from alturz.core.alturz_database.alturz_db_conf import get_log_channel
+from altruz import ALTRUZ
+from altruz.modules import *
+from altruz.core.startup_checks import check_or_set_log_channel, check_arq_api
+from altruz.core.altruz_database.altruz_db_conf import get_log_channel
 from config import Config
 
 
 async def main_startup():
     print("""
-|| AlturZ Userbot ||
-Copyright (c) 2021 TeamAlturZ
+|| AltruZ Userbot ||
+Copyright (c) 2021 TeamAltruZ
 """
     )
-    await ALTURZ.start()
+    await ALTRUZ.start()
     log_channel_id = await check_or_set_log_channel()
     await check_arq_api()
     try:
-        await ALTURZ.send_message(chat_id=log_channel_id[1], text="Master, \n`AlturZ is now Online")
+        await ALTRUZ.send_message(chat_id=log_channel_id[1], text="Master, \n`AltruZ is now Online")
     except:
         print("WARNING: There was an error while creating the LOG_CHANNEL please add a one manually!")
     await idle()
