5b913b37b691c98f79d892ed7630a3f5e0426012
XkshaT7678 <73880884+XkshaT7678@users.noreply.github.com>
2021-12-29 11:25:39 +0530

    Create pictool_help.py

---
+++ altruz/helpers/pictool_help.py
@@ -0,0 +1,13 @@
+from io import BytesIO
+from aiohttp import ClientSession
+
+
+# Aiohttlp Session
+aiosession = ClientSession()
+
+async def gib_carbon_sar(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
