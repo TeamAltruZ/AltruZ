3a01467bb8ccf1f72c7b20e0b72689a4e137e18f
XkshaT7678 <73880884+XkshaT7678@users.noreply.github.com>
2022-01-03 07:43:51 +0530

    Create short_link.py

---
+++ altruz/modules/short_link.py
@@ -0,0 +1,52 @@
+import requests
+import os
+
+from pyrogram import filters
+from pyrogram.types import Message
+from altruz import ALTRUZ, CMD_HELP
+from altruz.helpers.pyrogram_help import get_arg
+from altruz.core.main_cmd import altruz_on_cmd, e_or_r
+from config import Config
+
+
+# Help
+CMD_HELP.update(
+    {
+        "short_url": f"""
+**Short Url,**
+  ✘ `short` - To short long url using is.gd's free api
+**Example:**
+  ✘ `short`,
+   ⤷ Send command with url = `{Config.CMD_PREFIX}short https://google.com`
+   ⤷ Reply to a url message = `{Config.CMD_PREFIX}short` (Reply to a message with url)
+"""
+    }
+)
+
+mod_file = os.path.basename(__file__)
+
+def paste_isgd(url):
+    main_url = f"https://is.gd/create.php?format=json&url={url}"
+    pasted_url = requests.post(main_url)
+    json_data = pasted_url.json()
+    short_url = json_data['shorturl']
+    return short_url
+
+@altruz_on_cmd(command="short", modlue=mod_file)
+async def cutr_short(_, message: Message):
+    replied_msg = message.reply_to_message
+    short_msg = await e_or_r(altruz_message=message, msg_text="`Processing...`")
+    if replied_msg:
+        to_short = replied_msg.text
+    elif not replied_msg:
+        try:
+            to_short = get_arg(message)
+        except Exception as e:
+            await short_msg.edit(f"**Error:** `{e}`")
+            return
+    shoterned_url = paste_isgd(to_short)
+    try:
+        print(shoterned_url)
+        await short_msg.edit(f"**Successfully Shortened the Url** \n\n**Shortened Url:** {shoterned_url} \n**Original Url:** {to_short}", disable_web_page_preview=True)
+    except Exception as e:
+        await short_msg.edit(f"**Error:** `{e}`")
